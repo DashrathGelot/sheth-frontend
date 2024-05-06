24c2c9cd8c8baf893819b6a9fed20c4e904831bb
DilipGelot <dilipgelot16@gmail.com>
2024-05-06 20:04:03 +0530

    added button component

---
+++ src/app/components/Button.jsx
@@ -0,0 +1,17 @@
+const Button = () => {
+  return (
+    <>
+      <button className="bg-primary text-fifth p-3 rounded-md m-4">
+        Primary-btn
+      </button>
+      <button className="bg-secondary text-fifth p-3 rounded-md m-2">
+        Secondary-btn
+      </button>
+      <button className="bg-third text-fifth p-3 rounded-md m-2">
+        Third-btn
+      </button>
+    </>
+  );
+};
+
+export default Button;
