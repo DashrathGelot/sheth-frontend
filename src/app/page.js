c4107f5effb246499da14d315be280a93f59ed9c
DilipGelot <dilipgelot16@gmail.com>
2024-05-07 19:25:18 +0530

    Tile components  created

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
-import Image from "next/image";
-import Button from "./components/Button";
+// import Button from "./components/Button";
+import Tile from "./components/Tile";
+// import Testimg from "./assets/images/test.webp";
 
 export default function Home() {
   return (
     <main className="container">
-      <Button type={"primary"} />
-      <Button type={"secondary"} />
-      <Button type={"third"} />
-      <Button type={"add_cart"} />
-      <Button type={"checkout"} />
-      <Button type={"back-top"} />
+      <Tile
+        image={
+          "https://cdn.shopify.com/s/files/1/0250/5057/9028/files/WSP24_46342520_ESSENTIAL_RELAXED_TEE_TOFU_MED_01_0026.jpg?v=1706730186"
+        }
+        title={"this is title"}
+      />
     </main>
   );
 }
