c331b7f27bd6f8695dd87477e44c61a20dbeab60
DilipGelot <dilipgelot16@gmail.com>
2024-05-10 22:57:26 +0530

    tile size dynamically chnaged

--- src/app/components/Tile.jsx
+++ src/app/components/Tile.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Tile = ({ image, title }) => {
+const Tile = ({ image, title, sizeClass }) => {
   return (
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+    <div className={`w-full ${sizeClass} p-4`}>
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <img className="w-full h-auto" src={image} alt={title} />
       </div>
