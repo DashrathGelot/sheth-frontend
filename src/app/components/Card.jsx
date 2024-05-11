eb2d66182c3de8c2f9f8f8075d7e926e8bf3a1f6
DilipGelot <dilipgelot16@gmail.com>
2024-05-11 22:12:08 +0530

    comment resolved

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,14 +1,18 @@
-const Card = ({ image, title, price, Name }) => {
+import Tile from "./Tile";
+
+const Card = ({ image, title, price, name }) => {
   return (
     <>
       <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
         <div className="bg-white shadow-md rounded-lg overflow-hidden">
           <Tile image={image} title={title} />
           <div className="p-4">
-            <h2 className="font-bold text-lg">{Name}</h2>
-            <div className="flex justify-between items-center mt-2">
-              <span className="text-gray-700">₹{price}</span>
-            </div>
+            {name && <h2 className="font-bold text-lg">{name}</h2>}
+            {price && (
+              <div className="flex justify-between items-center mt-2">
+                <span className="text-gray-700">₹{price}</span>
+              </div>
+            )}
           </div>
         </div>
       </div>
