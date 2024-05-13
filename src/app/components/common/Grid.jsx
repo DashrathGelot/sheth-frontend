3a4bf9390af3558f5b5a71d8c657cf0d2caa2f03
DilipGelot <dilipgelot16@gmail.com>
2024-05-13 21:53:24 +0530

    Grid updated

--- src/app/components/common/Grid.jsx
+++ src/app/components/common/Grid.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
 const Grid = ({ gap, cols, children }) => {
-  const colClasses = `grid grid-cols-1 sm:grid-cols-${cols} md:grid-cols-${
-    cols * 2
-  } gap-${gap}`;
-  return <div className={colClasses}>{children}</div>;
+  return <div className={`grid grid-cols-1 sm:grid-cols-${cols} md:grid-cols-${cols * 2} gap-${gap}`}>{children}</div>;
 };
 
 export default Grid;
