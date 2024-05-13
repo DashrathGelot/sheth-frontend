8325c6ff755086d8d953e7e46b03550c7925b1dd
DilipGelot <dilipgelot16@gmail.com>
2024-05-13 10:28:46 +0530

    svg files are created, used in navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,4 @@
-// NavigationBar.js
-
 import React from "react";
-// import Humburger from "../assets/svg/Humburger";
 
 const Navbar = () => {
   return (
@@ -9,38 +6,14 @@ const Navbar = () => {
       {/* Hamburger Icon */}
       <div className="flex items-center">
         <button className="text-primary focus:outline-none ">
-          <svg
-            className="w-6 h-6"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M4 18L20 18"
-              stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-            />
-            <path
-              d="M4 12L20 12"
-              stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-            />
-            <path
-              d="M4 6L20 6"
-              stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-            />
-          </svg>
+          <img src="/burger-menu.svg" alt="" srcset="" className="w-6 h-6" />
         </button>
       </div>
 
       {/* Logo and Name (Centered) */}
       <div className="flex items-center justify-center">
         <img
-          src="https://img.freepik.com/free-vector/abstract-logo-triangular-shape_1043-1.jpg?t=st=1715395100~exp=1715398700~hmac=54d70a72941fc169987438815651d4310a21bd800a566ec96459ac7fa7911680&w=826"
+          src="https://shorturl.at/yIY46"
           alt="Logo"
           className="h-8 w-8 mr-2"
         />
@@ -50,52 +23,17 @@ const Navbar = () => {
       <div className="  md:flex items-center">
         {/* Search Icon */}
         <div className="hidden md:flex">
-          <button className="text-white focus:outline-none mr-2 flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              viewBox="0 0 50 50"
-              className="w-6 h-6"
-              fill="#0D1B2A"
-            >
-              <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
-            </svg>
+          <button className="text-primary focus:outline-none mr-2">
+            <img src="/search.svg" className="w-6 h-6" />
           </button>
         </div>
         {/* User Icon */}
         <button className="text-primary focus:outline-none mr-2">
-          <svg
-            className="w-6 h-6"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M20 21C20 19.6044 20 18.9067 19.8278 18.3389C19.44 17.0605 18.4395 16.06 17.1611 15.6722C16.5933 15.5 15.8956 15.5 14.5 15.5H9.5C8.10444 15.5 7.40665 15.5 6.83886 15.6722C5.56045 16.06 4.56004 17.0605 4.17224 18.3389C4 18.9067 4 19.6044 4 21M16.5 7.5C16.5 9.98528 14.4853 12 12 12C9.51472 12 7.5 9.98528 7.5 7.5C7.5 5.01472 9.51472 3 12 3C14.4853 3 16.5 5.01472 16.5 7.5Z"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <img src="/user.svg" className="w-6 h-6" />
         </button>
         {/* Cart Icon */}
         <button className="text-primary focus:outline-none mr-2">
-          <svg
-            className="w-6 h-6"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M15.9996 8C15.9996 9.06087 15.5782 10.0783 14.828 10.8284C14.0779 11.5786 13.0605 12 11.9996 12C10.9387 12 9.92131 11.5786 9.17116 10.8284C8.42102 10.0783 7.99959 9.06087 7.99959 8M3.63281 7.40138L2.93281 15.8014C2.78243 17.6059 2.70724 18.5082 3.01227 19.2042C3.28027 19.8157 3.74462 20.3204 4.33177 20.6382C5.00006 21 5.90545 21 7.71623 21H16.283C18.0937 21 18.9991 21 19.6674 20.6382C20.2546 20.3204 20.7189 19.8157 20.9869 19.2042C21.2919 18.5082 21.2167 17.6059 21.0664 15.8014L20.3664 7.40138C20.237 5.84875 20.1723 5.07243 19.8285 4.48486C19.5257 3.96744 19.0748 3.5526 18.5341 3.29385C17.92 3 17.141 3 15.583 3L8.41623 3C6.85821 3 6.07921 3 5.4651 3.29384C4.92433 3.5526 4.47349 3.96744 4.17071 4.48486C3.82689 5.07243 3.76219 5.84875 3.63281 7.40138Z"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <img src="/cart.svg" className="w-6 h-6" />
         </button>
       </div>
     </nav>
