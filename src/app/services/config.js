550be819fdb2fffb1877a3020f599556ac873ce9
DilipGelot <dilipgelot16@gmail.com>
2024-06-05 15:18:51 +0530

    Product Page created

--- src/app/services/config.js
+++ src/app/services/config.js
@@ -1,50 +1,64 @@
 export const config = {
-    navbar: {
-      logo: "https://shorturl.at/yIY46",
-      text: "SHETHH",
+  navbar: {
+    logo: "https://shorturl.at/yIY46",
+    text: "SHETHH",
+  },
+  footer: [
+    {
+      title: "Column 1",
+      links: [
+        {
+          text: "Our Client Advisors are available to assist you by phone at +1.866.VUITTON . You can also chat or email us.",
+          url: "#",
+        },
+        { text: "FAQs", url: "#" },
+        { text: "Product Care", url: "#" },
+        { text: "Stores", url: "#" },
+      ],
     },
-    footer:  [
+    {
+      title: "Column 2",
+      links: [
+        { text: "Repairs", url: "#" },
+        { text: "Personalization", url: "#" },
+        { text: "Art of Gifting", url: "#" },
+        { text: "Download our Apps", url: "#" },
+      ],
+    },
+    {
+      title: "Column 3",
+      links: [
+        { text: "Fashion Shows", url: "#" },
+        { text: "Arts & Culture", url: "#" },
+        { text: "La Maison", url: "#" },
+        { text: "Sustainability", url: "#" },
+        { text: "Latest News", url: "#" },
+      ],
+    },
+    {
+      title: "Column 4",
+      links: [
         {
-          title: "Column 1",
-          links: [
-            {
-              text: "Our Client Advisors are available to assist you by phone at +1.866.VUITTON . You can also chat or email us. ",
-              url: "#",
-            },
-            { text: "FAQs", url: "#" },
-            { text: "Product Care", url: "#" },
-            { text: "Stores", url: "#" },
-          ],
+          text: "Sign up for Louis Vuitton emails and receive the latest news from the Maison, including exclusive online pre-launches and new collections.",
+          url: "#",
         },
-        {
-          title: "Column 2",
-          links: [
-            { text: "Repairs", url: "#" },
-            { text: "Personalization", url: "#" },
-            { text: "Art of Gifting", url: "#" },
-            { text: "Download our Apps", url: "#" },
-          ],
-        },
-        {
-          title: "Column 3",
-          links: [
-            { text: "Fashion Shows", url: "#" },
-            { text: "Arts & Culture", url: "#" },
-            { text: "La Maison", url: "#" },
-            { text: "Sustainability", url: "#" },
-            { text: "Latest News", url: "#" },
-          ],
-        },
-        {
-          title: "Column 4",
-          links: [
-            {
-              text: "Sign up for Louis Vuitton emails and receive the latest news from the Maison, including exclusive online pre-launches and new collections.",
-              url: "#",
-            },
-            { text: "Follow Us", url: "#" },
-          ],
-        }
+        { text: "Follow Us", url: "#" },
+      ],
+    }
+  ],
+  productDetails: {
+    name: "Men Striped Polo Neck Polyester Light Green T-Shirt",
+    price: "$465.00",
+    colors: ["Light Blue", "Red", "Green", "Pink"],  
+    sizes: ["S", "M", "L", "XL"],     
+    images: [
+      "https://shorturl.at/evS12",
+      "https://shorturl.at/PITZR"
+    ],
+    accordions: [
+      { title: "Accordion Title 1 ", details: "This is details for accordion 1." },
+      { title: "Accordion Title 2", details: "This is details for accordion 2." },
+      { title: "Accordion Title 3", details: "This is details for accordion 3." },
     ]
+  }
 };
-
