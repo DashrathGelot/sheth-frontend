315b4c4e6614a89037f0a1465ff9df4a08314f2e
DilipGelot <dilipgelot16@gmail.com>
2024-06-16 08:45:50 +0530

    Product List page created

--- src/app/services/config.js
+++ src/app/services/config.js
@@ -1,50 +1,99 @@
 export const config = {
-    navbar: {
-      logo: "https://shorturl.at/yIY46",
-      text: "SHETHH",
+  navbar: {
+    logo: "https://shorturl.at/yIY46",
+    text: "SHETHH",
+  },
+  footer: [
+    {
+      title: "Column 1",
+      links: [
+        {
+          text: "Our Client Advisors are available to assist you by phone at +1.866.VUITTON . You can also chat or email us.",
+          url: "#",
+        },
+        { text: "FAQs", url: "#" },
+        { text: "Product Care", url: "#" },
+        { text: "Stores", url: "#" },
+      ],
     },
-    footer:  [
+    {
+      title: "Column 2",
+      links: [
+        { text: "Repairs", url: "#" },
+        { text: "Personalization", url: "#" },
+        { text: "Art of Gifting", url: "#" },
+        { text: "Download our Apps", url: "#" },
+      ],
+    },
+    {
+      title: "Column 3",
+      links: [
+        { text: "Fashion Shows", url: "#" },
+        { text: "Arts & Culture", url: "#" },
+        { text: "La Maison", url: "#" },
+        { text: "Sustainability", url: "#" },
+        { text: "Latest News", url: "#" },
+      ],
+    },
+    {
+      title: "Column 4",
+      links: [
         {
-          title: "Column 1",
-          links: [
-            {
-              text: "Our Client Advisors are available to assist you by phone at +1.866.VUITTON . You can also chat or email us. ",
-              url: "#",
-            },
-            { text: "FAQs", url: "#" },
-            { text: "Product Care", url: "#" },
-            { text: "Stores", url: "#" },
-          ],
+          text: "Sign up for Louis Vuitton emails and receive the latest news from the Maison, including exclusive online pre-launches and new collections.",
+          url: "#",
         },
-        {
-          title: "Column 2",
-          links: [
-            { text: "Repairs", url: "#" },
-            { text: "Personalization", url: "#" },
-            { text: "Art of Gifting", url: "#" },
-            { text: "Download our Apps", url: "#" },
-          ],
-        },
-        {
-          title: "Column 3",
-          links: [
-            { text: "Fashion Shows", url: "#" },
-            { text: "Arts & Culture", url: "#" },
-            { text: "La Maison", url: "#" },
-            { text: "Sustainability", url: "#" },
-            { text: "Latest News", url: "#" },
-          ],
-        },
-        {
-          title: "Column 4",
-          links: [
-            {
-              text: "Sign up for Louis Vuitton emails and receive the latest news from the Maison, including exclusive online pre-launches and new collections.",
-              url: "#",
-            },
-            { text: "Follow Us", url: "#" },
-          ],
-        }
-    ]
+        { text: "Follow Us", url: "#" },
+      ],
+    },
+  ],
+  products: [
+    {
+      image: 'https://shorturl.at/hUm9i',
+      title: 'Cotton And Linen Blend Tailored Flared Pants',
+      price: '1,470.00',
+      name: 'Product 1',
+    },
+    {
+      image: 'https://shorturl.at/hUm9i',
+      title: 'Printed Viscose Blouson',
+      price: '3,800.00',
+      name: 'Product 2',
+    },
+    {
+      image: 'https://shorturl.at/hUm9i',
+      title: 'Patchwork Long-Sleeved Denim Polo Shirt',
+      price: '3,300.00',
+      name: 'Product 3',
+    },
+    {
+      image: 'https://shorturl.at/hUm9i',
+      title: 'Striped Patchwork Denim Shorts',
+      price: '2,330.00',
+      name: 'Product 4',
+    },
+    {
+      image: 'https://shorturl.at/hUm9i',
+      title: 'Striped Patchwork Denim Shorts',
+      price: '2,330.00',
+      name: 'Product 4',
+    },
+    {
+      image: 'https://shorturl.at/hUm9i',
+      title: 'Striped Patchwork Denim Shorts',
+      price: '2,330.00',
+      name: 'Product 4',
+    },
+    {
+      image: 'https://shorturl.at/hUm9i',
+      title: 'Striped Patchwork Denim Shorts',
+      price: '2,330.00',
+      name: 'Product 4',
+    },
+    {
+      image: 'https://shorturl.at/hUm9i',
+      title: 'Striped Patchwork Denim Shorts',
+      price: '2,330.00',
+      name: 'Product 4',
+    },
+  ]
 };
-
