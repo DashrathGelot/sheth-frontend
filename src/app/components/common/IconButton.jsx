1daed913ee230a02023879dbddea01ba8ea9fd99
Dilip Gelot <dilipgelot16@gmail.com>
2024-11-30 09:58:41 +0530

    Svg File chnaged for filter icon, IconButton  margin right decreased after text on right side icon

--- src/app/components/common/IconButton.jsx
+++ src/app/components/common/IconButton.jsx
@@ -1,7 +1,7 @@
 const IconButton = ({ iconSrc, altText, onClick, left, right, className }) => {
   return (
     <button id="iconButton" className={`text-primary focus:outline-none pl-3 flex items-center justify-center ${className && className}`} onClick={onClick}>
-      {right && <span className="mr-4">{right}</span>}
+      {right && <span className="mr-2">{right}</span>}
       <img src={iconSrc} alt={altText} className="w-5 h-5" /> 
       {left && <span className="ml-6">{left}</span>}
     </button>
