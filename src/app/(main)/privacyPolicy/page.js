3b3113e559de5870669653df41f11e5733c22042
Hardik <malihardik88@gmail.com>
2025-02-21 00:17:43 +0530

    Privacy-Policy Page

---
+++ src/app/(main)/privacyPolicy/page.js
@@ -0,0 +1,216 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="min-h-screen bg-white">
+      <div className="relative w-full px-4 py-12 sm:px-6 lg:px-8">
+        <h1 className="mb-8 text-center mx-auto text-4xl font-bold tracking-tight text-gray-900">
+          Privacy Policy
+        </h1>
+
+        <div className="absolute prose prose-gray mx-auto  max-w-none left:0">
+          <p className="text-sm text-gray-500">Last Updated: Feb 20, 2025</p>
+
+          <p className="mt-6 text-[14px]">
+            This Privacy Policy ("Policy") outlines how RollyRich ("we," "our,"
+            or "us") collects, uses, shares, and protects your personal
+            information when you interact with our website. It also explains
+            your rights regarding your personal data and how you can exercise
+            them.
+          </p>
+
+          <br />
+
+          <p className="text-[14px]">
+            By accessing or using our Platform or engaging with us in any way,
+            you acknowledge that you have read and understand this Policy.
+          </p>
+
+          <br />
+
+          <ul className="list-disc space-y-2 pl-6">
+            <li className="text-[14px]">
+              We do not store your credit card, payment-instrument information.
+            </li>
+            <li className="text-[14px]">
+              We will not rent or sell your name or personal information.
+            </li>
+            <li className="text-[14px]">
+              We utilize cookies to help RollyRich to recognize you as a typed
+              user only.
+            </li>
+          </ul>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            1. Information We Collect
+          </h2>
+          <br />
+          <p className="text-[14px]">
+            We collect only the information essential for our business
+            operations, including providing services, processing your order,
+            updating you on its status, and contacting you regarding relevant
+            marketing or additional services offered by us or considered
+            relevant to our services.
+          </p>
+
+          <br />
+
+          <p className="text-[14px]">
+            We collect personal information from you when you interact with our
+            website, such as when you:
+          </p>
+          <br />
+          <ul className="list-disc space-y-2 pl-6">
+            <li className="text-[14px]">Create an account</li>
+            <li className="text-[14px]">Make a purchase</li>
+            <li className="text-[14px]">Contact customer support</li>
+          </ul>
+
+          <p className="mt-4 font-semibold text-[16px]">
+            Types of Personal Information We Collect:
+          </p>
+          <br />
+          <ol type="i" className="list-disc space-y-2 pl-6 roman">
+            <li className="text-[14px]">
+              Identifiers: first name, last name, email address, phone number,
+              shipping address
+            </li>
+            <li className="text-[14px]">
+              Account Information: Username, password, and order history
+            </li>
+            <li className="text-[14px]">
+              Payment Information: processed securely through third-party
+              payment providers
+            </li>
+            <li className="text-[14px]">
+              Purchase History: Details of products purchased and returns
+            </li>
+          </ol>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            2. How We Collect Information
+          </h2>
+          <br />
+          <ul className="list-disc space-y-2 pl-6">
+            <li className="text-[14px]">
+              Directly from you – When you create an account, make a purchase,
+              or contact us.
+            </li>
+            <li className="text-[14px]">
+              From third parties – Such as oauth login service.
+            </li>
+          </ul>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            3. How We Use Your Information
+          </h2>
+          <br />
+          <p>We use your personal information for the following purposes:</p>
+          <br />
+          <ul className="list-disc space-y-2 pl-6">
+            <li className="text-[14px]">
+              To process and fulfill your orders (including payment, shipping,
+              and delivery).
+            </li>
+            <li className="text-[14px]">
+              To personalize your shopping experience and recommend products.
+            </li>
+            <li className="text-[14px]">
+              To provide customer service and respond to inquiries.
+            </li>
+            <li className="text-[14px]">
+              To send promotional emails, newsletters, and special offers (you
+              can opt out anytime).
+            </li>
+            <li className="text-[14px]">
+              To detect and prevent fraudulent transactions.
+            </li>
+            <li className="text-[14px]">
+              To comply with legal obligations and enforce our policies.
+            </li>
+          </ul>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            4. How We Share Your Information
+          </h2>
+          <br />
+          <p className="text-[14px]">We may share your information with:</p>
+          <br />
+          <ul className="list-disc space-y-2 pl-6">
+            <li className="text-[14px]">
+              Service Providers – Payment processors, shipping carriers, and
+              marketing partners.
+            </li>
+            <li className="text-[14px]">
+              We do not sell your personal information.
+            </li>
+          </ul>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            5. How We Protect Your Information
+          </h2>
+          <br />
+          <p className="text-[14px]">
+            We implement security measures to protect your personal information,
+            including encryption, secure servers, and access restrictions.
+            However, no method of transmission is 100% secure, so we encourage
+            you to take precautions with your account.
+          </p>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            6. Your Rights & Choices
+          </h2>
+          <br />
+          <p className="text-[14px]">
+            Depending on your location, you may have the right to:
+          </p>
+          <br />
+          <ul className="list-disc space-y-2 pl-6">
+            <li className="text-[14px]">
+              Access the personal data we hold about you.
+            </li>
+            <li className="text-[14px]">
+              Correct inaccurate or incomplete information.
+            </li>
+            <li className="text-[14px]">Delete your personal information.</li>
+            <li className="text-[14px]">
+              Opt-out of marketing communications.
+            </li>
+            <li className="text-[14px]">
+              Restrict or object to certain data processing.
+            </li>
+          </ul>
+          <br />
+          <p className="text-[14px]">
+            To exercise your rights, contact us at contact@rollyrich.com.
+          </p>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            7. Cookies & Tracking Technologies
+          </h2>
+          <br />
+          <p className="text-[14px]">
+            We use cookies and similar technologies to enhance your browsing
+            experience. You can manage your cookie preferences through your
+            browser settings.
+          </p>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            8. Third-Party integration
+          </h2>
+          <br />
+          <p className="text-[14px]">
+            Our website has only secure payment processor integration.
+          </p>
+
+          <h2 className="mt-8 text-2xl font-semibold text-gray-900">
+            9. Changes to This Policy
+          </h2>
+          <br />
+          <p className="text-[14px]">
+            We may update this Privacy Policy from time to time. Any changes
+            will be posted on this page with the updated date.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
